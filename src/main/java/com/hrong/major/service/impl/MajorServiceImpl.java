75d57951a73b1332e8fa8badd49c681ce452627b
huangrong <1011486768@qq.com>
2019-08-20 11:42:00 +0800

    feat:增加管理界面，完善majors、detail界面及后台

--- src/main/java/com/hrong/major/service/impl/MajorServiceImpl.java
+++ src/main/java/com/hrong/major/service/impl/MajorServiceImpl.java
@@ -1,10 +1,14 @@
 package com.hrong.major.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hrong.major.model.Major;
 import com.hrong.major.dao.MajorMapper;
 import com.hrong.major.service.MajorService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class MajorServiceImpl extends ServiceImpl<MajorMapper, Major> implements MajorService {
 
+	@Resource
+	private MajorMapper majorMapper;
+	@Override
+	public List<Major> findAroundMajors(Integer id) {
+		Major major = majorMapper.selectById(id);
+		List<Integer> ids = majorMapper.findAroundMajorIds(id, major.getSubjectId());
+		return majorMapper.selectList(new QueryWrapper<Major>().in("id", ids));
+	}
 }
