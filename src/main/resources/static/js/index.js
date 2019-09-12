9a03e9d36ad0d7dad993324db766d42fecd15c4a
huangrong <1011486768@qq.com>
2019-09-12 21:31:13 +0800

    feat

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,13 +1,16 @@
 function searchHot(name,formId) {
+    var id = 'form'+formId;
     $.ajax({
         type: "POST",   //提交的方法
         url:"/majors", //提交的地址
-        data:$('#form'+formId).serialize(),// 序列化表单值
+        data: $('#'+id).serialize(),// 序列化表单值
         async: false,
         error: function(request) {  //失败的话
+            alert(2)
             // alert("Connection error");
         },
         success: function(data) {  //成功
+            alert(1)
             // alert(data);  //就将返回的数据显示出来
             // window.location.href="跳转页面"
         }
